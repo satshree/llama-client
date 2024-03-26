899681a66ae3694f1410b60ac3c5833313e1c798
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-26 11:39:38 -0500

    define types

---
+++ src/types/state.ts
@@ -0,0 +1,40 @@
+import {
+  BillType,
+  CartType,
+  ProductType,
+  UserType,
+  WishlistType,
+} from "./models";
+
+/**
+ * AUTHENTICATION TYPES
+ */
+
+export type AuthTokenStateType = {
+  access: string;
+  refresh: string;
+};
+
+export type AuthUserStateType = {
+  id: string;
+  username: string;
+  super: boolean;
+};
+
+export interface AuthStateType {
+  token: AuthTokenStateType;
+  user: AuthUserStateType;
+}
+
+/**
+ * GLOBAL/ROOT STATE
+ */
+
+export interface GlobalState {
+  auth: AuthStateType;
+  user: UserType;
+  products: ProductType[];
+  cart: CartType;
+  wishlist: WishlistType;
+  userBills: BillType[];
+}
